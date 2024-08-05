e4ff4a50b45eb1a9b358c43c809c36aec4d9f8fb
iturhs20 <155578005+CreativeTecscapes@users.noreply.github.com>
2024-08-05 12:44:34 +0530

    Navbar created

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar"
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+       <Navbar/>
+    </main>
   );
 }
